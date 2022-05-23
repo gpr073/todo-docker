441184c2e0939e34061c032498252e9e9717fc85
gpr073 <lonewolf41320@gmail.com>
2022-05-23 19:27:26 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://admin:password@localhost:27017/todoDB?authSource=admin');
+
+const todosSchema = {
+    todo: String
+}
+
+const Todo = mongoose.model("Todo", todosSchema);
+
+app.get('/', async(req, res) => {
+    const allTodo = await Todo.find();
+    res.render('index', {todosList: allTodo})
+})
+
+app.post('/add/todo', function(req, res) {
+    let newTodo = new Todo({
+        todo: req.body.todo
+    });
+    newTodo.save().then(() => {
+        console.log("Successfully inserted");
+        res.redirect('/');
+    }).catch((err) => console.log(err));
+})
+
+app.get("/delete/todo/:_id", (req, res) => {
+    const {_id} = req.params;
+    Todo.deleteOne({_id}).then(() => {
+        console.log("Deleted successfully");
+        res.redirect("/");
+    }).catch((err) => console.log(err));
+})
+
+app.listen(4000, () => console.log('Server is running'))
